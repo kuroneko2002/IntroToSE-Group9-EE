7c3ce946c6f7444f74c854b86f4241fce9132a2a
20120109 <huy183605@gmail.com>
2022-12-27 18:26:02 +0700

    update all func

--- models/Archive_m.py
+++ models/Archive_m.py
@@ -1,19 +1,36 @@
 import CRUD_db
+import json
 
-def archiveTest(file):
+def archiveFormat(db_name, collection, file):
     try:
-        return 'Succeed!'
+        f = open(file)
+        data = json.load(f)
     except:
-        return 'Error!'
+        return 'Error'
+
+    # CRUD_db.deleteAll(db_name, collection)
+     
+    for obj in data:
+        res = CRUD_db.insert_db(db_name, collection, obj)
+        if (res == 'Error'): 
+            return res
+    
+    return 'Succeed'
+
+
+def archiveTest(type, part, file):
+    if (type == "listening"):
+        return archiveFormat("ListeningTest", part, file)
+        
+    elif (type == "reading"):
+        return archiveFormat("ReadingTest", part, file)
+        
+    else:
+        return 'Error'
 
 def archiveShortStory(file):
-    try:
-        return 'Succeed!'
-    except:
-        return 'Error!'
-
-def archiveVocab(file):
-    try:
-        return 'Succeed!'
-    except:
-        return 'Error!'
+    return archiveFormat("ShortStories", "Short Stories", file)
+    
+def archiveVocab(topic, file):
+    return archiveFormat("TopicVocab", topic, file)
+    
