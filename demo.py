e0ad4b299cb7b0521b392cae840d47a268a7df5a
20120109 <huy183605@gmail.com>
2022-12-28 22:31:16 +0700

    some testing :>

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import pymongo
 password='group9'
 #connect to cluster in MongoDB
 connection_string = f'mongodb+srv://group9:{password}@cluster0.yorkz9p.mongodb.net/?retryWrites=true&w=majority'
-client= pymongo.MongoClient(connection_string)
+# client= pymongo.MongoClient(connection_string)
 
 # #connect to Account Database
 # db=client["Account"]
